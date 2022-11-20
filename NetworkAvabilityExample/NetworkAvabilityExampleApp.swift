9fadfbc837be4316059b9d5ae78089eddfd74e9a
Mateja Opacic <mateja.opacic@gmail.com>
2022-11-20 17:17:08 +0100

    Initial Commit

---
+++ NetworkAvabilityExample/NetworkAvabilityExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkAvabilityExampleApp.swift
+//  NetworkAvabilityExample
+//
+//  Created by Mateja Opacic on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkAvabilityExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
